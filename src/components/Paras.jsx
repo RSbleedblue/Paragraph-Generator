334d71a7f2091b8db9a0468609cc523bc41002a5
Rivansh <rivansh63@gmail.com>
2024-05-09 10:19:17 +0530

    UI change

--- src/components/Paras.jsx
+++ src/components/Paras.jsx
@@ -1,7 +1,7 @@
 import para from "../utils/data";
 const Paras = ({data}) => {
     const paragraphs = para.slice(0,data).map((para,index) => (
-        <div key = {index} className="w-[100%] p-4 text-justify font-mono text-white shadow-xl rounded-lg bg-rose-400 bg-opacity-90 hover:cursor-pointer hover:scale-110 transition-all delay-100 ease-in-out " style={{ backdropFilter: 'blur(12px)' }}>
+        <div key = {index} className="w-[100%] p-4 text-justify font-mono text-gray-900 shadow-xl rounded-lg bg-slate-200 bg-opacity-90 hover:cursor-pointer hover:scale-110 transition-all delay-100 ease-in-out " style={{ backdropFilter: 'blur(12px)' }}>
                <span className="text-xl">{index+1}.</span>  {para}
         </div>
     ))
